04cf67bf977d1b89a4636c899424b308c88c1cc8
Justin Lee <jhankilee@gmail.com>
2016-01-13 23:40:45 -0800

    Initial Commit

---
+++ SwiftHelper/Extensions/UIColorExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColorExtensions.swift
+//  SwiftHelper
+//
+//  Created by Justin on 1/12/16.
+//
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    //TO_DO
+    //RandomColor
+    //Color RGB components & alpha
+
+}

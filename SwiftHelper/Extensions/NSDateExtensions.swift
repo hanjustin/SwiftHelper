0aee7235fab09d1f61d0c22855d54c2529800b38
Justin Lee <justin@Stevens-MacBook-Pro.local>
2016-01-14 23:52:43 -0800

    Updated NSDateExtensions
    Added:
    - yesterdayMidnight
    - dateComponents
    - numberOfdaysSince

--- SwiftHelper/Extensions/NSDateExtensions.swift
+++ SwiftHelper/Extensions/NSDateExtensions.swift
@@ -8,22 +8,42 @@
 
 import Foundation
 
-// TO_DO
-// date components (day, month, year, hour)
-// numberOfdaysSince(date)
-
 extension NSDate {
     
-    public func midnight(timezone: NSTimeZone = NSTimeZone.localTimeZone()) -> NSDate {
-        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-        calendar.timeZone = timezone
+    private var defaultCalendar: NSCalendar {
+        return NSCalendar.currentCalendar()
+    }
+    
+    private var defaultTimeZone: NSTimeZone {
+        return NSTimeZone.localTimeZone()
+    }
+    
+    
+    public func midnight(timezone: NSTimeZone? = nil) -> NSDate {
+        let calendar = defaultCalendar
+        calendar.timeZone = timezone ?? defaultTimeZone
         return calendar.startOfDayForDate(self)
     }
     
-    public func isToday(timezone: NSTimeZone = NSTimeZone.localTimeZone()) -> Bool {
+    public func isToday(timezone: NSTimeZone? = nil) -> Bool {
         return self.midnight(timezone) == NSDate().midnight(timezone)
     }
     
+    public func yesterdayMidnight(timezone: NSTimeZone? = nil) -> NSDate {
+        return self.midnight(timezone).dateByAddingTimeInterval(-60 * 60 * 24)
+    }
+    
+    public func dateComponents(timezone: NSTimeZone? = nil) -> (year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) {
+        let calendar = defaultCalendar
+        calendar.timeZone = timezone ?? defaultTimeZone
+        
+        let components = calendar.components([.Year, .Month, .Day, .Hour, .Minute, .Second], fromDate: self)
+        return (components.year, components.month, components.day, components.hour, components.minute, components.second)
+    }
+    
+    public func numberOfdaysSince(date: NSDate, timezone: NSTimeZone? = nil) -> Int {
+        return self.dateComponents(timezone).day - date.dateComponents(timezone).day
+    }
 }
 
 
